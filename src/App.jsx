a87e3d0e356990ba1fa539245ec0285f1fcd0d6b
Aditya Sakhuja <sakhujaaditya56@gmail.com>
2025-07-30 12:55:28 +0530

    Initial commit by Aditya

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Background from "./Components/Background";
+import Front from "./Components/Front";
+import "./App.css";
+import {useState} from "react";
+function App() {
+  var indata = [
+    {
+      title: "Aditya",
+      desc: "Hey Welcome to the Today-Task .Here you can add you daily Task ..",
+    },
+  ];
+  const[data,setData]=useState(indata);
+  
+
+  var onDelete=(cardTitle)=>{
+    console.log(`${cardTitle}Deleted..`);
+    var temp=data.filter((f)=> f.title!==cardTitle)
+    setData(temp);
+  }
+  return (
+    <div className="back fixed w-screen h-screen bg-zinc-700">
+      <Background />
+      <Front data={data} onDelete={onDelete}/>
+    </div>
+  );
+}
+
+export default App;
